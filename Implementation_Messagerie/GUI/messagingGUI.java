3b482b57ddfe05b62cedf62d26ffa05b22e7d88c
Quintus618 <qdouarre@etud.insa-toulouse.fr>
2021-12-02 21:08:24 +0100

    Interface Messagerie

--- Implementation_Messagerie/GUI/messagingGUI.java
+++ Implementation_Messagerie/GUI/messagingGUI.java
@@ -1,66 +1,35 @@
 package GUI;
 //Importation Libraries
 import javax.swing.*;
-import java.awt.Image;
-import java.awt.Color;
+import java.awt.*;
+import java.lang.*;
 
 public class messagingGUI extends JFrame{
 
+    //Buttons of the instant messaging
     private JButton deconnexionButton;
     private JButton mediaButton;
     private JButton fileButton;
     private JButton sendMessageButton;
-    
+
+    //Panels of the instant messaging
+    private JPanel chatPanel;
+    private JPanel infoProfilPanel;
+    private JPanel connectedPanel;
+    private JPanel messagePanel;
+
     //Constructor
     public messagingGUI(int height, int width){
         
         //Creation of GUI
         super("Insatact");
         setSize(width, height);
+        this.setLayout(new BorderLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add buttons
-        deconnexionButton = new JButton("deconnexion");
+        //Creation of graphical components 
+        buildComponentInterface(this);
 
-        //Add Pictures, Audio
-        ImageIcon iconMedia = new ImageIcon("./GUI/Pictures/media.png");
-        mediaButton = new JButton(iconMedia);
-        mediaButton.setIcon(resizeIcon(iconMedia, 100, 100));
-        mediaButton.setBackground(new Color(255,255,255,0));
-        mediaButton.setBorderPainted(false);
-        mediaButton.setContentAreaFilled(false);
-        mediaButton.setFocusPainted(false);
-
-
-        //Add files
-        ImageIcon iconFile = new ImageIcon("./GUI/Pictures/file.png");
-        fileButton = new JButton(iconFile);
-        fileButton.setIcon(resizeIcon(iconFile, 100, 100));
-        fileButton.setBackground(new Color(255,255,255,0));
-        fileButton.setBorderPainted(false);
-        fileButton.setContentAreaFilled(false);
-        fileButton.setFocusPainted(false);
-
-        //Send Messages
-        ImageIcon iconMessage = new ImageIcon("./GUI/Pictures/message.png");
-        sendMessageButton = new JButton(iconMessage);
-        sendMessageButton.setIcon(resizeIcon(iconMessage, 100, 100));
-        sendMessageButton.setBackground(new Color(255,255,255,0));
-        sendMessageButton.setBorderPainted(false);
-        sendMessageButton.setContentAreaFilled(false);
-        sendMessageButton.setFocusPainted(false);
-
-        //Texte Input zone to send messages
-        JTextArea textSenderZone = new JTextArea("Message à saisir"); 
-
-        JPanel panel = new JPanel();
-        panel.add(deconnexionButton);
-        panel.add(mediaButton);
-        panel.add(fileButton);
-        panel.add(sendMessageButton);
-        panel.add(textSenderZone);
-        add(panel);
-        setVisible(true);
     }
 
     //Redimensionner une icone
@@ -68,6 +37,63 @@ public class messagingGUI extends JFrame{
         Image img = icon.getImage();  
         Image resizedImage = img.getScaledInstance(resizedWidth, resizedHeight,  java.awt.Image.SCALE_SMOOTH);  
         return new ImageIcon(resizedImage);
+    }
+
+    //Parameters Buttons
+    private void paramButton(JButton b, int resizedWidth, int resizedHeight, ImageIcon i){
+        b.setIcon(resizeIcon(i, resizedWidth, resizedHeight));
+        b.setBackground(new Color(255,255,255,0));
+        b.setBorderPainted(false);
+        b.setContentAreaFilled(false);
+        b.setFocusPainted(false);
+    }
+
+    private void buildComponentInterface(JFrame f){
+        
+        //Add buttons
+        deconnexionButton = new JButton("deconnexion");
+
+        //Add Pictures, Audio
+        ImageIcon iconMedia = new ImageIcon("./GUI/Pictures/media.png");
+        mediaButton = new JButton(iconMedia);
+        paramButton(mediaButton, 40, 40, iconMedia);
+
+        //Add files
+        ImageIcon iconFile = new ImageIcon("./GUI/Pictures/file.png");
+        fileButton = new JButton(iconFile);
+        fileButton.setIcon(resizeIcon(iconFile, 40, 40));
+        paramButton(fileButton, 40, 40, iconFile);
+
+        //Send Messages
+        ImageIcon iconMessage = new ImageIcon("./GUI/Pictures/message.png");
+        sendMessageButton = new JButton(iconMessage);
+        paramButton(sendMessageButton, 40, 40, iconMessage);
+
+        //Texte Input zone to send messages
+        JTextArea textSenderZone = new JTextArea(2,80);
+        textSenderZone.setLineWrap (true);
+        textSenderZone.setWrapStyleWord (false);
+
+        chatPanel = new JPanel();
+        chatPanel.setBackground(Color.gray);
+        chatPanel.add(mediaButton);
+        chatPanel.add(fileButton);
+        chatPanel.add(textSenderZone);
+        chatPanel.add(sendMessageButton);
+        chatPanel.add(deconnexionButton);
+        add(chatPanel, BorderLayout.SOUTH);
+
+        connectedPanel = new JPanel();
+        connectedPanel.setLayout(new BoxLayout(connectedPanel, BoxLayout.Y_AXIS));
+        connectedPanel.setBackground(Color.decode("#2F2F2F"));
+        JLabel lconnecte = new JLabel("Liste des personnes connectées");
+        lconnecte.setForeground(Color.WHITE);
+        lconnecte.setFont(new Font("Serif", Font.PLAIN, 20));
+        connectedPanel.add(lconnecte);
+        JButton b = new JButton("Pseudodel'user");
+        connectedPanel.add(b);
+        add(connectedPanel, BorderLayout.EAST);
+        setVisible(true);
     }
     public static void main(String[] Args){
         messagingGUI mGUI = new messagingGUI(3000,2000);
