060fdbb74f0356321e10b647831a3eb6f7796653
Quintus618 <qdouarre@etud.insa-toulouse.fr>
2021-12-02 09:51:29 +0100

    Changement de nom de l'interface d'accueil

--- Implementation_Messagerie/GUI/messagingGUI.java
+++ Implementation_Messagerie/GUI/messagingGUI.java
@@ -16,13 +16,21 @@ public class messagingGUI extends JFrame{
         super("Insatact");
         setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
 
         //Add buttons
         deconnexionButton = new JButton("deconnexion");
+        deconnexionButton.setSize(1000, 50);
+
         mediaButton = new JButton("media");
         fileButton = new JButton("file");
         sendMessage = new JButton("sendMessage");
+        JPanel panel = new JPanel();
+        panel.add(deconnexionButton);
+        panel.add(mediaButton);
+        panel.add(fileButton);
+        panel.add(sendMessage);
+        add(panel);
+        setVisible(true);
     }
 
     public static void main(String[] Args){
