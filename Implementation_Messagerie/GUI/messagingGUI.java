ee809f38293a4e92f4f6d4ea2602e1a151a77e98
Quintus618 <qdouarre@etud.insa-toulouse.fr>
2021-12-02 09:29:51 +0100

    messaging interface

--- Implementation_Messagerie/GUI/messagingGUI.java
+++ Implementation_Messagerie/GUI/messagingGUI.java
@@ -3,13 +3,26 @@ package GUI;
 import javax.swing.*;
 
 public class messagingGUI extends JFrame{
+
+    private JButton deconnexionButton;
+    private JButton mediaButton;
+    private JButton fileButton;
+    private JButton sendMessage;
     
     //Constructor
     public messagingGUI(int height, int width){
+        
+        //Creation of GUI
         super("Insatact");
         setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+        //Add buttons
+        deconnexionButton = new JButton("deconnexion");
+        mediaButton = new JButton("media");
+        fileButton = new JButton("file");
+        sendMessage = new JButton("sendMessage");
     }
 
     public static void main(String[] Args){
