de439a350cc9132c8f3a5d6087177f9d5dce8620
Quintus618 <qdouarre@etud.insa-toulouse.fr>
2021-12-02 09:20:01 +0100

    Interface graphique

--- Implementation_Messagerie/GUI/messagingGUI.java
+++ Implementation_Messagerie/GUI/messagingGUI.java
@@ -1,5 +1,19 @@
 package GUI;
+//Importation Libraries
+import javax.swing.*;
 
-public class messagingGUI {
+public class messagingGUI extends JFrame{
     
+    //Constructor
+    public messagingGUI(int height, int width){
+        super("Insatact");
+        setSize(width, height);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] Args){
+        messagingGUI mGUI = new messagingGUI(3000,2000);
+    }
+
 }
