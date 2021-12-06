76e2f0002736216fc2b7633d04dcbb469c7ed7c7
Pierre Favary <favary@insat.insa-toulouse.fr>
2021-12-06 09:13:30 +0100

    push en retard

--- Implementation_Messagerie/GUI/welcomGUI.java
+++ Implementation_Messagerie/GUI/welcomGUI.java
@@ -14,6 +14,9 @@ public class welcomGUI extends JFrame{
     private JButton inscriptionButton;
     private JLabel logo;
     private JLabel texte;
+    private JPanel panel;
+    private JPanel buttonsPanel;
+    private Container ContentPane;
     
     //Constructor
     public welcomGUI(int height, int width){
@@ -27,23 +30,32 @@ public class welcomGUI extends JFrame{
         inscriptionButton = new JButton("Inscription");
         connexionButton = new JButton("Connexion");
         //très sale
-        logo = new JLabel(new ImageIcon(new ImageIcon("./GUI/Pictures/chat.png").getImage().getScaledInstance(180, 180, 10)));//à quoi sert le troisième int?
+        logo = new JLabel(new ImageIcon(new ImageIcon("./GUI/Pictures/chat.png").getImage().getScaledInstance(180, 180, java.awt.Image.SCALE_SMOOTH)));//à quoi sert le troisième int?
+        //voir quels critères de privacy pour appeler resizeIcon de welcomGUI (créer un .java de méthodes réutilisables partout type "webtools.java"?)
         texte= new JLabel("Bienvenue!");
 
         texte.setForeground(Color.CYAN);
         texte.setFont(new Font("Serif", Font.BOLD, 40));
 
         setVisible(true);
-        JPanel panel = new JPanel();
+
+        buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        buttonsPanel.add(connexionButton);
+        buttonsPanel.add(inscriptionButton);
+
+        panel = new JPanel();
         panel.setBackground(Color.BLACK);
         panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
         panel.setAlignmentX(Component.CENTER_ALIGNMENT);
         panel.setAlignmentY(Component.CENTER_ALIGNMENT);//ne marchent pas, parce que.
         panel.add(logo);
         panel.add(texte);
-        panel.add(connexionButton);
-        panel.add(inscriptionButton);
-        add(panel);
+        panel.add(buttonsPanel);
+//et je ne sais pas si ça marche car montp n'a pas le bon JDK, bien sûr
+        contentPane = getContentPane();
+        contentPane.add(panel, BorderLayout.CENTER);
+        contentPane.add(buttonsPanel, BorderLayout.PAGE_END);
     }
 
 
