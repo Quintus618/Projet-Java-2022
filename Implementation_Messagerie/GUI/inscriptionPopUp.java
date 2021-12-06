e0ca0bc920f4151ec7f65eabc8d6ae447a9cdd77
Quintus618 <qdouarre@etud.insa-toulouse.fr>
2021-12-06 15:37:26 +0100

    Poursuite Fenetre

--- Implementation_Messagerie/GUI/inscriptionPopUp.java
+++ Implementation_Messagerie/GUI/inscriptionPopUp.java
@@ -1,7 +1,7 @@
 package GUI;
 
 import javax.swing.*;
-import javax.swing.*;
+import javax.swing.SpringLayout;
 import java.awt.*;
 import java.lang.*;
 
@@ -21,22 +21,37 @@ public class inscriptionPopUp extends JFrame {
 
         //Creation panel
         inscriptionPanel = new JPanel();
-        inscriptionPanel.setLayout(new GridLayout(2,2));
+        inscriptionPanel.setLayout(new SpringLayout());
         
         //Creation des labels
-        JLabel ID = new JLabel("ID: ");
-        JLabel Mdp = new JLabel("Mot de passe: ");
-        JTextField idText = new JTextField();
-        JTextField mdpText = new JTextField();
+        JLabel ID = new JLabel("ID: ", JLabel.TRAILING);
+        JLabel Mdp = new JLabel("Mot de passe: ", JLabel.TRAILING);
+        JTextField idText = new JTextField(10);
+        JTextField mdpText = new JTextField(10);
+        Mdp.setLabelFor(mdpText);
+        ID.setLabelFor(idText);
         inscriptionPanel.add(ID);
         inscriptionPanel.add(idText);
         inscriptionPanel.add(Mdp);
         inscriptionPanel.add(mdpText);
         add(inscriptionPanel);
+
+        SpringUtilities.makeCompactGrid(inscriptionPanel,
+                                        2, 2, //rows, cols
+                                        6, 6,        //initX, initY
+                                        6, 6);       //xPad, yPad
+
+        JButton incr = new JButton("Inscription");
+        add(incr, BorderLayout.SOUTH);
         setVisible(true);
     }
 
+
     public static void main(String[] Args){
-        inscriptionPopUp iPP = new inscriptionPopUp(300, 700);
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                inscriptionPopUp iPP = new inscriptionPopUp(300, 700);
+            }
+        });
     }
 }
