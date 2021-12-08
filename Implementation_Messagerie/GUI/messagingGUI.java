cd932043f7d0fec48f5ec7c68e21fcf9e4d70682
Quintus618 <qdouarre@etud.insa-toulouse.fr>
2021-12-08 12:07:39 +0100

    Gestion envoi message

--- Implementation_Messagerie/GUI/messagingGUI.java
+++ Implementation_Messagerie/GUI/messagingGUI.java
@@ -1,6 +1,7 @@
 package GUI;
 //Importation Libraries
 import javax.swing.*;
+import java.awt.event.*;
 import java.awt.*;
 import java.lang.*;
 
@@ -23,9 +24,13 @@ public class messagingGUI extends JFrame{
 
     private GridBagConstraints c = new GridBagConstraints();
 
+    private JTextArea textSenderZone;
+
     private String pseudo;
     private int numberLine = 0;
     private int numberMessage = 0;
+
+    private GridBagLayout gl;
 
     //Constructor
     public messagingGUI(int height, int width){
@@ -79,7 +84,7 @@ public class messagingGUI extends JFrame{
         paramButton(sendMessageButton, 40, 40, iconMessage);
 
         //Texte Input zone to send messages
-        JTextArea textSenderZone = new JTextArea(2,80);
+        textSenderZone = new JTextArea(2,80);
         textSenderZone.setLineWrap (true);
         textSenderZone.setWrapStyleWord (false);
 
@@ -109,16 +114,16 @@ public class messagingGUI extends JFrame{
 
         //Messages zone
         messagePanel = new JPanel();
-        messagePanel.setLayout(new GridLayout(numberLine,2));
+        gl = new GridBagLayout();
+        messagePanel.setLayout(gl);
+        JScrollPane scrollPane = new JScrollPane(messagePanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setPreferredSize(new Dimension(20,10));
+        add(scrollPane, BorderLayout.WEST);
 
         //FAIRE UN SCROLL PLUTOT
+        /*receiveMessage();
         receiveMessage();
-        receiveMessage();
-        receiveMessage();
-        receiveMessage();
-        receiveMessage();
-        receiveMessage();
-        receiveMessage();receiveMessage();
         receiveMessage();
         writeMessage();
         receiveMessage();
@@ -130,6 +135,10 @@ public class messagingGUI extends JFrame{
         receiveMessage();
         writeMessage();
         receiveMessage();
+        receiveMessage();
+        receiveMessage();
+        writeMessage();
+        receiveMessage();*/
 
        /* SpringUtilities.makeCompactGrid(messagePanel,
         10, 2, //rows, cols
@@ -138,44 +147,113 @@ public class messagingGUI extends JFrame{
 
         add(messagePanel, BorderLayout.CENTER);
 
+
+        //ACTION BOUTTON
+        sendMessageButton.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){ writeMessage(textSenderZone.getText());}});
+        
         setVisible(true);
     }
 
-    private void writeMessage(){
+    private void writeMessage(String t){
         numberMessage++;
-        numberLine = numberMessage % MAX_MESS;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        //numberLine = numberMessage % MAX_MESS;
+        //c.fill = GridBagConstraints.HORIZONTAL;
         JPanel MEnv = new JPanel();
-        JLabel message1 = new JLabel("Message envoyé");
+        JLabel message1 = new JLabel(t);
         MEnv.setBackground(Color.decode("#7F7FBF"));
+        MEnv.setPreferredSize(new Dimension(750, 100));
         MEnv.add(message1);
         JPanel MBlanc = new JPanel();
-        c.gridx = numberLine;
-        c.gridy = 0;
+        MBlanc.setPreferredSize(new Dimension(750, 100));
+        c.gridx = 0;
+        c.gridy = numberMessage;
         messagePanel.add(MEnv,c);
-        c.gridx = numberLine;
-        c.gridy = 1;
+        c.gridx = 1;
+        c.gridy = numberMessage;
         messagePanel.add(MBlanc,c);
+        textSenderZone.setText("");
+        SwingUtilities.updateComponentTreeUI(this);
     }
 
     private void receiveMessage(){
+
         numberMessage++;
         numberLine = numberMessage % MAX_MESS;
-        c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.VERTICAL;
         JPanel MRec = new JPanel();  
         JLabel message2 = new JLabel("Message reçu");
         MRec.setBackground(Color.decode("#7FBF7F"));
+        MRec.setPreferredSize(new Dimension(750, 100));
         MRec.add(message2);
         JPanel MBlanc = new JPanel();
-        c.gridx = numberLine;
-        c.gridy = 0;
+        MBlanc.setPreferredSize(new Dimension(750, 100));
+        c.gridx = 0;
+        c.gridy = numberMessage;
         messagePanel.add(MBlanc,c);
-        c.gridx = numberLine;
-        c.gridy = 1;
+        c.gridx = 1;
+        c.gridy = numberMessage;
         messagePanel.add(MRec,c);
+        SwingUtilities.updateComponentTreeUI(this);
     }
-    public static void main(String[] Args){
+    public static void main(String[] Args) throws InterruptedException{
         messagingGUI mGUI = new messagingGUI(3000,2000);
+        Thread.sleep(5000);
+        System.out.println("Toto");
+        //System.out.println(mGUI.gl.getRows());
+        /*mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.writeMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.writeMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.writeMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.writeMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.writeMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.writeMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        mGUI.receiveMessage();
+        SwingUtilities.updateComponentTreeUI(mGUI);
+        Thread.sleep(5000);
+        //System.out.println(mGUI.gl.getRows());
+        */
     }
 
 }
