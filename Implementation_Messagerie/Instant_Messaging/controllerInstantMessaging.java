e2200ece14ce24c47c254010462f474587c2e80b
Favary Pierre <favary@insa-20665>
2021-12-09 10:02:39 +0100

    init tables

--- Implementation_Messagerie/Instant_Messaging/controllerInstantMessaging.java
+++ Implementation_Messagerie/Instant_Messaging/controllerInstantMessaging.java
@@ -14,10 +14,8 @@ public class controllerInstantMessaging{
 
  public controllerInstantMessaging(String potato){ 
      try {
-
-       //String drv = "com.mysql.jdbc.Driver";
-       //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-    //Class.forName(drv);//woops
+//nécessité de rajouter les .jar de /Library dans le classPath
+//VScode: Java projects en bas à gauche, clic droit sur Implementation_messagerie, configure class path
     Class.forName("com.mysql.cj.jdbc.Driver");
 
     lien = DriverManager.getConnection(addresse,login,pwd);
@@ -28,16 +26,18 @@ public class controllerInstantMessaging{
 
     System.out.println(potato);
 
-    //la commande sql n'est probablement pas bonne
-    /*String commande = "CREATE TABLE IF NOT EXISTS Users (id VARCHAR(63) NOT NULL, pseudo VARCHAR(63), password VARCHAR(63) NOT NULL, PRIMARY KEY (id));";
+    //intialisation, à modifier
+    String createUsers = "CREATE TABLE IF NOT EXISTS Users (id VARCHAR(63) NOT NULL, pseudo VARCHAR(63), password VARCHAR(63) NOT NULL, PRIMARY KEY (id));";
+    String createArchives = "CREATE TABLE IF NOT EXISTS Archives (idsrc VARCHAR(63) NOT NULL, iddest VARCHAR(63) NOT NULL, message VARCHAR(8000), chrono TIME, PRIMARY KEY (idsrc,iddest));";
     try {
         Statement statem = lien.createStatement();
-        statem.execute(commande);
+        statem.execute(createUsers);
+        statem.execute(createArchives);
         statem.close();
     } catch (SQLException e) {
         System.out.println(e.getMessage());
         e.printStackTrace();
-    }*/
+    }
 
     try {
         lien.close();
