27f30fa41e7bdaf82738c391ebb1f9fe3a7e6e17
Favary Pierre <favary@insa-20665>
2021-12-09 10:51:54 +0100

    commandes BDD v0.1

--- Implementation_Messagerie/Instant_Messaging/controllerInstantMessaging.java
+++ Implementation_Messagerie/Instant_Messaging/controllerInstantMessaging.java
@@ -11,11 +11,27 @@ public class controllerInstantMessaging{
     //incidemment le login est aussi le nom de la BDD
     private String addresse = "jdbc:mysql://srv-bdens.insa-toulouse.fr:3306/"+login;//?useSSL=false ?
     private Connection lien;
+    //y accéder via terminal: mysql -h srv-bdens.insa-toulouse.fr -P 3306 -D tp_servlet_003 -u tp_servlet_003 -pulah5Bee
+    //puis show tables; (toutes les commandes mysql doivent finir par ;)(pas d'espace entre le -p et le mdp)
 
- public controllerInstantMessaging(String potato){ 
-     try {
+
+ public controllerInstantMessaging(){ 
+
+    //intialisations, à modifier
+    String createUsers = "CREATE TABLE IF NOT EXISTS Users (id VARCHAR(63) NOT NULL, password VARCHAR(63) NOT NULL, PRIMARY KEY (id));";
+    String createArchives = "CREATE TABLE IF NOT EXISTS Archives (id1 VARCHAR(63) NOT NULL, id2 VARCHAR(63) NOT NULL, message VARCHAR(8000), chrono TIME, PRIMARY KEY (id1,id2));";
+    //gérer ensuite les archivages indépendemment de la source et du destinataire
+    String[] initialisation={createUsers,createArchives};
+
+    askBDD(initialisation);
+
+
+}
+
+private void ouvrir(){
 //nécessité de rajouter les .jar de /Library dans le classPath
 //VScode: Java projects en bas à gauche, clic droit sur Implementation_messagerie, configure class path
+try {
     Class.forName("com.mysql.cj.jdbc.Driver");
 
     lien = DriverManager.getConnection(addresse,login,pwd);
@@ -23,22 +39,9 @@ public class controllerInstantMessaging{
         System.out.println(e.getMessage());
         e.printStackTrace();
     }
+}
 
-    System.out.println(potato);
-
-    //intialisation, à modifier
-    String createUsers = "CREATE TABLE IF NOT EXISTS Users (id VARCHAR(63) NOT NULL, pseudo VARCHAR(63), password VARCHAR(63) NOT NULL, PRIMARY KEY (id));";
-    String createArchives = "CREATE TABLE IF NOT EXISTS Archives (idsrc VARCHAR(63) NOT NULL, iddest VARCHAR(63) NOT NULL, message VARCHAR(8000), chrono TIME, PRIMARY KEY (idsrc,iddest));";
-    try {
-        Statement statem = lien.createStatement();
-        statem.execute(createUsers);
-        statem.execute(createArchives);
-        statem.close();
-    } catch (SQLException e) {
-        System.out.println(e.getMessage());
-        e.printStackTrace();
-    }
-
+private void fermer(){
     try {
         lien.close();
     } catch (SQLException e) {
@@ -46,7 +49,39 @@ public class controllerInstantMessaging{
         e.printStackTrace();
     }
 }
+
+private void demander(String[] demandes){
+    try {
+        Statement statem = lien.createStatement();
+        for (int i=0;i<demandes.length;i++){
+            statem.execute(demandes[i]);
+        }
+        statem.close();
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+        e.printStackTrace();
+    }
+
+}
+
+protected void askBDD(String[] requetes){
+    ouvrir();
+    demander(requetes);
+    fermer();
+}
+
+//PAS ENCORE TESTEE 
+protected void addUser(String pseudo, String id){
+    String insertion = "INSERT INTO Users VALUES ("+pseudo+","+id+");";
+    askBDD(new String[] {insertion});
+}
+
     public static void main(String[] Args){
-            controllerInstantMessaging test= new controllerInstantMessaging("potato");
+            controllerInstantMessaging test= new controllerInstantMessaging();
+            System.out.println("Succès de la création des tables initiales.");
+            String delAllUsers = "DROP TABLE Users;";
+            String delAllArchives = "DROP TABLE Archives;";
+            test.askBDD(new String[] {delAllUsers,delAllArchives});
+            System.out.println("Succès de la suppression des tables initiales.");
         }
     }
