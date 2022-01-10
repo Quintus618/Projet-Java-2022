0dc0cb10b8a6aadae8e2bf251a0d7dd8a5a7427d
Favary Pierre <favary@insa-20069>
2022-01-10 16:22:40 +0100

    woopsie oublie de save conversation

--- Implementation_Messagerie/Instant_Messaging/Conversation.java
+++ Implementation_Messagerie/Instant_Messaging/Conversation.java
@@ -77,6 +77,10 @@ public class Conversation extends JLabel{
     public usertype getCorrespondant() {
         return correspondant;
     }
+    
+    public void setCorrespondant(usertype neousr){
+        this.correspondant=neousr;
+    }
 
     public TCPcontrollerClient getTCP() {
         return TCP;
